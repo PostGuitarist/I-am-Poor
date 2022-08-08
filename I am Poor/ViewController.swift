e7723869a5a2b0d8f785dc8f54110e7d65ae4e13
Zaden Connell <zaden.connell@gmail.com>
2022-08-08 09:14:15 -0400

    Initial commit

---
+++ I am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Poor
+//
+//  Created by Zaden Connell on 8/4/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
